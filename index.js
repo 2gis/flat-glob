8140887a8121dfcde20e1c565d8f5c0dc1d5bfd1
Artem Zanko <a.zanko@2gis.ru>
2016-03-23 14:53:01 +0600

    flat glob rework

--- index.js
+++ index.js
@@ -1,49 +1,59 @@
 var glob = require("glob");
-var flatten = require("flatten-array");
-var uniques = require("uniques");
-var iter = require("iter");
 
-module.exports = async;
-module.exports.sync = sync;
+exports.sync = function sync(arr) {
+    var result = [];
+    arr = flatten(arr);
 
-function async (arr, callback) {
-  var result = [];
-  arr = flatten(arr);
+    var i = -1;
+    var len = arr.length;
 
-  iter(arr.length)
-    .done(function () {
-      callback(undefined, uniques(flatten(result)));
-    })
-    .run(function (next, i) {
-      if (arr[i].indexOf('*') == -1) {
-        result.push(arr[i]);
-        return next();
-      }
+    while (++i < len) {
+        if (arr[i].indexOf('*') == -1) {
+            result.push(arr[i]);
+            continue;
+        }
 
-      glob(arr[i], function (error, files) {
-        if (error) return callback(error);
-
-        result.push(files);
-        next();
-      });
-    });
-}
-
-function sync (arr) {
-  var result = [];
-  arr = flatten(arr);
-
-  var i = -1;
-  var len = arr.length;
-
-  while (++i < len) {
-    if (arr[i].indexOf('*') == -1) {
-      result.push(arr[i]);
-      continue;
+        result.push(glob.sync(arr[i]));
     }
 
-    result.push(glob.sync(arr[i]));
-  }
+    return unique(flatten(result));
+};
 
-  return uniques(flatten(result));
+function unique(arr) {
+    var dict = {};
+    var result = [];
+
+    var i = -1, len = arr.length;
+    while (++i < len) {
+        if (dict[arr[i]]) {
+            continue;
+        }
+
+        dict[arr[i]] = true;
+        result.push(arr[i]);
+    }
+
+    return result;
+}
+
+function flatten(list, result) {
+    if(!Array.isArray(list)) {
+        return list;
+    }
+
+    var i = -1;
+    var len = list.length;
+
+    result || (result = []);
+
+    while (++i < len) {
+        if (!Array.isArray(list[i])) {
+            result.push(list[i]);
+            continue;
+        }
+
+        flatten(list[i], result);
+    }
+
+    return result;
 }
